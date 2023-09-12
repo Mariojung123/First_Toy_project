7cebc80b4e106dff8e0222c048f8a4aead1dc625
Mariojung123 <idim06268@gmail.com>
2023-09-12 17:28:03 +0900

    test

--- Toy_project_1/stack.cpp
+++ Toy_project_1/stack.cpp
@@ -1,47 +1,68 @@
-#include <iostream>
-#include <stack>
+#include<iostream>
+#include<stack>
+
 using namespace std;
 
-int main() {
+int main(){
     string a;
     stack<char> st;
+    
 
-    while (true) {
-        getline(cin, a);
-        
-        if (a == ".") {
+    while(a!="."){
+        while(!st.empty()){ //스택 초기화
+            st.pop();
+            // printf("cleared!__first\n"); //확인문자
+        }
+        getline(cin,a);
+        if(a.empty()) continue; //수정부분
+        if(a=="."){ //끝내기
             break;
         }
-
-        for (int i = 0; i < a.length(); i++) {
-            if (a[i] == '(' || a[i] == '[') {
+        for(int i=0; i < a.size(); i++){
+            if(a[i]=='(' || a[i]=='['){
                 st.push(a[i]);
-            } else if (a[i] == ')') {
-                if (st.empty() || st.top() != '(') {
-                    printf("no\n");
-                    goto end;
-                }
-                st.pop();
-            } else if (a[i] == ']') {
-                if (st.empty() || st.top() != '[') {
-                    printf("no\n");
-                    goto end;
-                }
-                st.pop();
+                // printf("%c push\n",a[i]);
             }
-        }
 
-        if (st.empty()) {
-            printf("yes\n");
-        } else {
-            printf("no\n");
-        }
+            if(a[i]==')'){
+                if(st.empty()){
+                    printf("no\n");
+                    break;
+                }
+                else if(st.top()=='('){
+                    st.pop();
+                    // printf("%c pop\n",a[i]);
+                }
+                else if(st.top()=='['){
+                    printf("no\n");
+                    break;
+                }
+            }
+            if(a[i]==']'){
+                if(st.empty()){
+                    printf("no\n");
+                    break;
+                }
+                else if(st.top()=='['){
+                    st.pop();
+                    // printf("%c pop",a[i]);
+                }
+                else if(st.top()=='('){
+                    printf("no\n");
+                    break;
+                }
+            }
+            if(a[i]=='.'){ //마침표가 찍히고 스텍이 남아있을 때
+                if(!st.empty()){
+                    printf("no\n");
+                    break;
+                }
+                else if(st.empty()){
+                    printf("yes\n");
+                }
+            }
 
-        end:
-        while (!st.empty()) {
-            st.pop();
         }
     }
-
-    return 0;
+return 0;
 }
--- Toy_project_1/test.cpp
+++ Toy_project_1/test.cpp
@@ -1,60 +1,13 @@
-#include<iostream>
-#include<stack>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main(){
-    string a;
-    stack<char> st;
-    
-
-    while(a!="."){
-        while(!st.empty()){ //스택 초기화
-            st.pop();
-            // printf("cleared!__first\n"); //확인문자
-        }
-        getline(cin,a);
-        if(a=="."){ //끝내기
-            printf("end\n");
-            break;
-        }
-        for(int i=0; i < a.size(); i++){
-            if(a[i]=='(' || a[i]=='['){
-                st.push(a[i]);
-                // printf("%c push\n",a[i]);
-            }
-
-            if(a[i]==')'){
-                if(st.empty()){
-                    printf("no_1\n");
-                    break;
-                }
-                else if(st.top()=='('){
-                    st.pop();
-                    // printf("%c pop\n",a[i]);
-                }
-            }
-            if(a[i]==']'){
-                if(st.empty()){
-                    printf("no_2\n");
-                    break;
-                }
-                else if(st.top()=='['){
-                    st.pop();
-                    // printf("%c pop",a[i]);
-                }
-            }
-            if(a[i]=='.'){ //마침표가 찍히고 스텍이 남아있을 때
-                if(!st.empty()){
-                    printf("no_3\n");
-                    break;
-                }
-                else if(st.empty()){
-                    printf("yes_4\n");
-                }
-            }
-
-        }
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    while(scoville[0]<K){
+        
     }
-return 0;
+
+return answer;
 }
