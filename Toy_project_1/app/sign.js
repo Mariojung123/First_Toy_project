56fc684c49180607f06f0f804ce8395eaff7b882
Mariojung123 <idim06268@gmail.com>
2023-09-25 20:06:59 +0900

    키 숨기기
    Feat:

--- Toy_project_1/app/sign.js
+++ Toy_project_1/app/sign.js
@@ -1,10 +1,9 @@
 'use strict';
 
 const sign = {
-  GOOGLE_CLIENT_ID:
-    '385599561868-87msd9mhtljpgg8ckq5lgpk1ot841tkp.apps.googleusercontent.com',
-  GOOGLE_CLIENT_SECRET: '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',
-  GOOGLE_REDIRECT_URI: 'http://localhost:3000/oauth/redirect',
+  GOOGLE_CLIENT_ID: '.',
+  GOOGLE_CLIENT_SECRET: '/',
+  GOOGLE_REDIRECT_URI: '.',
 };
 
 module.exports = sign;
