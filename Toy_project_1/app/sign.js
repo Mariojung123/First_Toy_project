ab50be24f1ce62cc86169343ccb39e5588c6779f
Mariojung123 <115441849+Mariojung123@users.noreply.github.com>
2023-10-06 15:01:39 +0900

    Update sign.js

--- Toy_project_1/app/sign.js
+++ Toy_project_1/app/sign.js
@@ -2,9 +2,9 @@
 
 const sign = {
   GOOGLE_CLIENT_ID:
-    '385599561868-87msd9mhtljpgg8ckq5lgpk1ot841tkp.apps.googleusercontent.com',
-  GOOGLE_CLIENT_SECRET: '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',
-  GOOGLE_REDIRECT_URI: 'http://localhost:3000/oauth/redirect',
+    '보지',
+  GOOGLE_CLIENT_SECRET: '마세요',
+  GOOGLE_REDIRECT_URI: '뒤1지기 싫으면',
 };
 
 module.exports = sign;
