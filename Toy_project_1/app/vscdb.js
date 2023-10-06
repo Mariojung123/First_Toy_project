533058f5298eeb0d00812194cdc26ba9a3382565
Mariojung123 <idim06268@gmail.com>
2023-10-06 11:43:06 +0900

    env파일로 감추기

--- Toy_project_1/app/vscdb.js
+++ Toy_project_1/app/vscdb.js
@@ -1,14 +1,16 @@
-const mysql = require('mysql');
+require('dotenv').config();
 
+const mysql = require('mysql');
 const db = mysql.createConnection({
   //아래 코드가 기존 코드이고 이거는 잠깐 실험해볼라고 적어놓은 코드임
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'login',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = db;
+
 // const dbInfo = {
 //   host: 'localhost',
 //   user: 'root',
