82ae01078a3fa2a579bf98b7276b907591cd2c51
Mariojung123 <115441849+Mariojung123@users.noreply.github.com>
2023-10-06 15:02:14 +0900

    Update vscdb.js

--- Toy_project_1/app/vscdb.js
+++ Toy_project_1/app/vscdb.js
@@ -10,22 +10,3 @@ const db = mysql.createConnection({
 });
 
 module.exports = db;
-
-// const dbInfo = {
-//   host: 'localhost',
-//   user: 'root',
-//   password: '1410bbcc??',
-//   database: 'login',
-// };
-
-// module.exports = {
-//   init: function () {
-//     return mysql.createConnection(dbInfo);
-//   },
-//   connect: function (conn) {
-//     conn.connect(function (err) {
-//       if (err) console.error('mysql 연결 에러 : ' + err);
-//       else console.log('mysql 연결 성공');
-//     });
-//   },
-// };
