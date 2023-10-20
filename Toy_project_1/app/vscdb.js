5bec9ed1289e275a1b6cc9b9c60d590b41b040ea
Mariojung123 <idim06268@gmail.com>
2023-10-20 09:40:30 +0900

    mysql 연동 완료
    mysql2 사용해서 성공

--- Toy_project_1/app/vscdb.js
+++ Toy_project_1/app/vscdb.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const db = mysql.createConnection({
   //아래 코드가 기존 코드이고 이거는 잠깐 실험해볼라고 적어놓은 코드임
   host: process.env.DB_HOST,
